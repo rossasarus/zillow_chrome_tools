7475e1cfb66a2ed82ded7fc577fbb3f8af6d16f3
ross daly <rdaly@everquote.com>
2019-09-30 22:15:20 -0400

    init commit. price per square foot on refresh

---
+++ content.js
@@ -0,0 +1,74 @@
+var mutationObserver = new MutationObserver(function(mutations) {
+  // Stops the MutationObserver from listening for changes.
+  // mutationObserver.disconnect();
+  
+  console.log('mutation detected');
+  callback()
+
+    // // Starts listening for changes in the root HTML element of the page.
+    // mutationObserver.observe(document.documentElement, {
+    //   attributes: true,
+    //   characterData: true,
+    //   childList: true,
+    //   subtree: true,
+    //   attributeOldValue: true,
+    //   characterDataOldValue: true
+    // });
+
+});
+
+var callback = function(){
+    mutationObserver.disconnect();
+    let cards = document.getElementsByClassName('list-card-heading')
+    let regex_1 = /,/gi
+    for (let card of cards){
+      if (card.getElementsByClassName('list-card-details')[0].children.length == 3){
+        let price = card.getElementsByClassName('list-card-price')[0].innerText.replace(regex_1,'').replace('$','').replace('+','')
+        card.getElementsByClassName('list-card-price')[0].innerText = `$${price}`
+        let area = card.getElementsByClassName('list-card-details')[0].getElementsByTagName('li')[2].innerText.split(' ')[0].replace(',','')
+        try { card.getElementsByClassName('list-card-details')[0].getElementsByTagName('li')[0].getElementsByClassName('list-card-label')[0].innerText = 'bd' }
+        catch {
+          console.log('missing beds label')
+        }
+        try { card.getElementsByClassName('list-card-details')[0].getElementsByTagName('li')[1].getElementsByClassName('list-card-label')[0].innerText = 'ba' }
+        catch {
+          console.log('missing bath label')
+        }
+        try { card.getElementsByClassName('list-card-details')[0].getElementsByTagName('li')[2].getElementsByClassName('list-card-label')[0].innerText = 'sf' }
+        catch {
+          console.log('missing square feet label')
+        }
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(`${Math.round(price / area)}$/sf`);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        card.getElementsByClassName('list-card-details')[0].appendChild(node);     // Append <li> to <ul> with
+      }
+    }
+    // Starts listening for changes in the root HTML element of the page.
+    mutationObserver.observe(document.documentElement, {
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true,
+      attributeOldValue: true,
+      characterDataOldValue: true
+    });
+  };
+  
+  if (
+      document.readyState === "complete" ||
+      (document.readyState !== "loading" && !document.documentElement.doScroll)
+  ) {
+    mutationObserver.disconnect();
+    callback();
+    mutationObserver.observe(document.documentElement, {
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true,
+      attributeOldValue: true,
+      characterDataOldValue: true
+    });
+  } else {
+    document.addEventListener("DOMContentLoaded", callback);
+  }
---
+++ readme.md
@@ -0,0 +1,3 @@
+This extension adds links to s3 and auction insights on a cloudwatch page. 
+
+It can be installed by going to chrome://extensions/ in chrome, selecting developer mode, and then hitting the "Load Unpacked' button. Navigate to the plugin directory and activate it.
