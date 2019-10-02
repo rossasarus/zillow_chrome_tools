721281b90349e813b432039d5106308c55fb8f5e
ross daly <rdaly@everquote.com>
2019-10-02 15:51:03 -0400

    adding price per square ft to popup bubble.

--- content.js
+++ content.js
@@ -1,16 +1,17 @@
-// what to do when a mutation is detected.
+// Listen for mutations so we can check for new values to calc
 var mutationObserver = new MutationObserver(function(mutations) {
   console.log('mutation detected');
   callback()
 });
 
 var callback = function(){
-    // Stops the MutationObserver from listening for changes.
+    // Stop the MutationObserver from listening for changes.
     // Otherwise this snake will eat it's tail
     mutationObserver.disconnect();
     let cards = document.getElementsByClassName('list-card-heading')
     let regex_1 = /,/gi
     for (let card of cards){
+      // see if we have already modified this card.
       if (card.getElementsByClassName('list-card-details')[0].children.length == 3){
         let price = card.getElementsByClassName('list-card-price')[0].innerText.replace(regex_1,'').replace('$','').replace('+','')
         card.getElementsByClassName('list-card-price')[0].innerText = `$${price}`
@@ -30,10 +31,38 @@ var callback = function(){
         var node = document.createElement("LI");                 // Create a <li> node
         var textnode = document.createTextNode(`${Math.round(price / area)}$/sf`);         // Create a text node
         node.appendChild(textnode);                              // Append the text to <li>
-        card.getElementsByClassName('list-card-details')[0].appendChild(node);     // Append <li> to <ul> with
+        card.getElementsByClassName('list-card-details')[0].appendChild(node);     // Append <li> to <ul> 
       }
     }
-    // Starts / Restarts listening for changes.
+    try {
+      // check that this hasn't been added already
+      if (document.getElementsByClassName('mini-bubble-details')[0] != undefined){
+        let area = document.getElementsByClassName('mini-bubble-details')[0].getElementsByTagName('div')[1].innerText.split(' ')[0].replace(',','')
+        area = parseInt(area)
+        let price = document.getElementsByClassName('mini-bubble-details')[0].getElementsByTagName('strong')[0].innerText.split(' ')[0].replace('$','')
+        if (price.split('').pop() == 'K'){
+          price = parseInt(price.split('K')[0])*1000
+        } else if (price.split('').pop() == 'M'){
+          price = parseInt(price.split('K')[0])*1000000
+        }
+        // this is the old way. adding a node will make it easier to check if the $/sqft price has been added already
+        if (document.getElementsByClassName('mini-bubble-details')[0].getElementsByTagName('strong')[0].innerText.split('$/').length == 1){
+          document.getElementsByClassName('mini-bubble-details')[0].getElementsByTagName('strong')[0].innerText += ` ${Math.round(price/area)}$/sf  `
+        }
+        // this is the new way. this isn't working quite yet.
+        // if (document.getElementById('pricePerSquareFoot') == null){
+        //   var bubbleCalc = document.createElement("DIV");  
+        //   bubbleCalc.setAttribute("id", "pricePerSquareFoot")               // Create a <li> node
+        //   var bubbleTextNode = document.createTextNode(`${Math.round(price / area)}$/sf`);         // Create a text node
+        //   bubbleCalc.appendChild(bubbleTextNode);                              // Append the text to <li>
+        //   document.getElementsByClassName('mini-bubble-details')[0].appendChild(bubbleCalc);     // Append <li> to <ul> with
+        // }
+      }
+    }
+    catch (err){
+      console.log(err)
+    }
+    // Starts/Restarts listening for changes.
     mutationObserver.observe(document.documentElement, {
       attributes: true,
       characterData: true,
